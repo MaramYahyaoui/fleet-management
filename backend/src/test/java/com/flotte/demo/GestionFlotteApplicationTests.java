f0a70b65d923c441778d902b4048774d458a0528
maram yahyaoui <yahyaouimaram20@gmail.com>
2026-04-25 22:25:18 +0000

    test(backend): add unit tests

---
+++ backend/src/test/java/com/flotte/demo/GestionFlotteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flotte.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFlotteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
